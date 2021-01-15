d8fc45044118d4a855646526ede0881c990ba966
Pragati Gupta <73426124+pragatig746@users.noreply.github.com>
2021-01-16 00:48:47 +0530

    Create automation.py

---
+++ automation.py
@@ -0,0 +1,10 @@
+pip install pyautogui
+
+import pyautogui as pe
+import time
+import subprocess
+import os
+
+pe.click(1783,4,1),pe.moveTo(162,664,2),pe.click(162,664,2),time.sleep(10),pe.moveTo(122,138,1),time.sleep(2),pe.click(122,138,1),
+pe.typewrite('Sweet'),pe.moveTo(271,295,1),pe.click(271,295,1),pe.moveTo(780,988,2),pe.click(780,988,1),
+pe.typewrite('Good Morning Fam'),pe.press('enter'),pe.moveTo(1887,17,2),pe.click(1887,17,1)
